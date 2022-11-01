d582cf17c3717e6930ae18ae89cb816fec0fe455
matheusgondra <matheusgondra951@gmail.com>
2022-10-31 22:08:27 -0300

    Finalizando capitulo 10

--- ntask-api/middlewares.js
+++ ntask-api/middlewares.js
@@ -1,12 +1,14 @@
 const bodyParser = require("body-parser");
+const express = require("express");
 
 module.exports = app => {
 	app.set("port", 3000);
 	app.set("json spaces", 4);
 	app.use(bodyParser.json());
-	app.use(app.auth.initialize())
+	app.use(app.auth.initialize());
 	app.use((req, res, next) => {
 		delete req.body.id;
 		next();
-	})
+	});
+	app.use(express.static("public"));
 }
