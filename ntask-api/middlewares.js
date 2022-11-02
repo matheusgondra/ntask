651dd98c153908d44c1247abfe3624ed7059ebb6
matheusgondra <matheusgondra951@gmail.com>
2022-11-01 22:42:45 -0300

    Finalizando capitulo 11

--- ntask-api/middlewares.js
+++ ntask-api/middlewares.js
@@ -1,9 +1,26 @@
 const bodyParser = require("body-parser");
 const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const logger = require("./logger");
+const compression = require("compression");
+const helmet = require("helmet");
 
 module.exports = app => {
 	app.set("port", 3000);
 	app.set("json spaces", 4);
+	app.use(morgan("common", {
+		stream: {
+			write: (log) => logger.info(log)
+		}
+	}));
+	app.use(helmet());
+	app.use(cors({
+		origin: ["http://localhost:3001"],
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"]
+	}));
+	app.use(compression());
 	app.use(bodyParser.json());
 	app.use(app.auth.initialize());
 	app.use((req, res, next) => {
